3aa0b8b179dab766a6e5736fabb66c4993c27b1d
MhmmdRFadhil <fadhiill2719@gmail.com>
2021-11-07 11:09:19 +0800

    Initial commit

--- app/src/androidTest/java/com/ryz/moviecatalogue/ui/activity/MainActivityTest.kt
+++ app/src/androidTest/java/com/ryz/moviecatalogue/ui/activity/MainActivityTest.kt
@@ -16,13 +16,10 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import kotlin.math.abs
 
 class MainActivityTest {
     private val dummyMovies = DataDummy.getMovie()
     private val dummyTvShow = DataDummy.getTvShow()
-    private val dummyDetailMovie = DataDummy.getMovieDetail()
-    private val dummyDetailTvShow = DataDummy.getTvShowDetail()
 
     @get:Rule
     var activityRule = ActivityScenarioRule(MainActivity::class.java)
@@ -55,35 +52,18 @@ class MainActivityTest {
                 click()
             )
         )
-        onView(withId(R.id.tvTitleDetail)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(dummyDetailMovie.title)))
-        }
+        onView(withId(R.id.tvTitleDetail)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.tvGenreDetail)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(convertGenre(dummyDetailMovie.genre))))
-        }
+        onView(withId(R.id.tvGenreDetail)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.tvUserScoreDetail)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(dummyDetailMovie.score.toString())))
-        }
+        onView(withId(R.id.tvUserScoreDetail)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.tvDurationDetail)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(convertDuration(dummyDetailMovie.duration))))
-        }
+        onView(withId(R.id.tvDurationDetail)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.tvYearDetail)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(convertYears(dummyDetailMovie.years))))
-        }
+        onView(withId(R.id.tvYearDetail)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.tvOverviewDetail)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(convertOverview(dummyDetailMovie.overview))))
-        }
+        onView(withId(R.id.tvOverviewDetail)).check(matches(isDisplayed()))
+
     }
 
     @Test
@@ -104,91 +84,17 @@ class MainActivityTest {
                 click()
             )
         )
-        onView(withId(R.id.tvTitleDetail)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(dummyDetailTvShow.title)))
-        }
+        onView(withId(R.id.tvTitleDetail)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.tvGenreDetail)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(convertGenre(dummyDetailTvShow.genre))))
-        }
+        onView(withId(R.id.tvGenreDetail)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.tvUserScoreDetail)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(dummyDetailTvShow.score.toString())))
-        }
+        onView(withId(R.id.tvUserScoreDetail)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.tvDurationDetail)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(convertDuration(dummyDetailTvShow.duration))))
-        }
+        onView(withId(R.id.tvDurationDetail)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.tvYearDetail)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(convertYears(dummyDetailTvShow.years))))
-        }
+        onView(withId(R.id.tvYearDetail)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.tvOverviewDetail)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(convertOverview(dummyDetailTvShow.overview))))
-        }
-    }
+        onView(withId(R.id.tvOverviewDetail)).check(matches(isDisplayed()))
 
-    private fun convertOverview(overview: String?): String {
-        var tvOverview = ""
-
-        overview?.let {
-            tvOverview = if (it.isEmpty()) {
-                "No info"
-            } else {
-                it
-            }
-        }
-        return tvOverview
-    }
-
-    private fun convertDuration(duration: Int?): String {
-        var tvDuration = ""
-        duration?.let {
-            val hours = it / 60
-            val minutes = abs(it) % 60
-
-            tvDuration = when {
-                hours == 0 && minutes == 0 -> {
-                    "-"
-                }
-                hours == 0 -> {
-                    "$minutes m"
-                }
-
-                hours == 0 -> {
-                    "$hours h"
-                }
-
-                else -> {
-                    "$hours h $minutes m"
-                }
-            }
-        }
-        return tvDuration
-    }
-
-
-    private fun convertGenre(genre: List<String>?): String {
-        var tvGenre = ""
-        genre?.let {
-            tvGenre = if (it.isEmpty()) {
-                "No info"
-            } else {
-                it.toString().replace("[", "").replace("]", "")
-            }
-        }
-        return tvGenre
-    }
-
-    private fun convertYears(years: String?): String {
-        val date = years?.split("-")
-        return date?.get(0) ?: "-"
     }
 }
