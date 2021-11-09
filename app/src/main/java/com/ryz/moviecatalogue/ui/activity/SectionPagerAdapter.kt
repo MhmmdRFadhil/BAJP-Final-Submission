dd4e6fa7a24fd9843accdfc8235593adcfaa75e4
MhmmdRFadhil <fadhiill2719@gmail.com>
2021-11-10 06:22:03 +0800

    Implement advice from dicoding reviewer

--- app/src/main/java/com/ryz/moviecatalogue/ui/activity/SectionPagerAdapter.kt
+++ app/src/main/java/com/ryz/moviecatalogue/ui/activity/SectionPagerAdapter.kt
@@ -5,24 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.ryz.moviecatalogue.R
-import com.ryz.moviecatalogue.ui.content.movie.MovieFragment
-import com.ryz.moviecatalogue.ui.content.tvshow.TvShowFragment
+import com.ryz.moviecatalogue.ui.content.CatalogueFragment
 
 class SectionPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int = TAB_TITLES.size
 
     override fun createFragment(position: Int): Fragment {
-        var fragment: Fragment? = null
-        when (position) {
-            0 -> fragment = MovieFragment()
-            1 -> fragment = TvShowFragment()
-        }
-        return fragment as Fragment
+        return CatalogueFragment.newInstance(position)
     }
 
     companion object {
         @StringRes
         val TAB_TITLES = intArrayOf(R.string.menu_movies, R.string.menu_tv_show)
     }
-
 }
